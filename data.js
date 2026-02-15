65441d3c9e34469281b2c50251a1576f95d42cfe
blurpack <jooho08@naver.com>
2026-02-16 04:20:15 +0900

    Update data.js

--- data.js
+++ data.js
@@ -8,22 +8,22 @@ const myPortfolioData = {
             "img": "THIRSTY.webp", 
             "title": "THIRSTY", 
             "artist": "검정치마", 
-            "tracks": "틀린질문,Lester Burnham,섬 (Queen Of Diamonds),상수역,광견일기,Bollywood,빨간 나를,*Put Me On Drugs,*하와이 검은 모래,*맑고 묽게,*그늘은 그림자로,피와 갈증 (King Of Hurts)",
+            "tracks": "틀린질문|Lester Burnham|섬 (Queen Of Diamonds)|상수역|광견일기|Bollywood|빨간 나를|*Put Me On Drugs|*하와이 검은 모래|*맑고 묽게|*그늘은 그림자로|피와 갈증 (King Of Hurts)",
 			"author": "비스포크"
         },
-		{ 
+	    { 
             "img": "NOWITZKI.webp", 
             "title": "NOWITZKI", 
             "artist": "빈지노", 
-            "tracks": "*Stink Kiss (Intro),Monet,침대에서/막걸리,여행 Again (Feat. Causious Clay),*Dope As (Interlude),Coca Cola Red (Feat. oygli),990 (Feat. Kim Ximya),Lemon,바보같이 (Feat. Y2K92),*Trippy (Feat. Lance Skiiiwalker),*Crime (Bek Hyunjin 250),Camp,*Sanso (Interlude),Change,단 하루,*Sandman,Radio,*Gym",
+            "tracks": "*Stink Kiss (Intro)|Monet|침대에서/막걸리|여행 Again (Feat. Causious Clay)|*Dope As (Interlude)|Coca Cola Red (Feat. oygli)|990 (Feat. Kim Ximya)|Lemon|바보같이 (Feat. Y2K92)|*Trippy (Feat. Lance Skiiiwalker)|*Crime (Bek Hyunjin, 250)|Camp|*Sanso (Interlude)|Change|단 하루|*Sandman|Radio|*Gym",
 			"author": "BANA"
         }
     ],
     "movies": [
         {
-	    	"img": "이터널 선샤인.webp",
-	    	"title": "Eternal Sunshine of the Spotless Mind",
-	    	"author": "2004 Anonymous Content & This Is That Corp"
-		}
+	        "img": "이터널 선샤인.webp",
+	        "title": "Eternal Sunshine of the Spotless Mind",
+	        "author": "2004 Anonymous Content & This Is That Corp"
+        }
     ]
 };
